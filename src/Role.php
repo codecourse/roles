c7daa71e10f8770eeceeab5c9fa349dbaa3966d2
Alex Garrett <alex@codecourse.com>
2017-09-19 08:49:35 +0100

    Disable timestamps

--- src/Role.php
+++ src/Role.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    /**
+     * Disable timestamps.
+     *
+     * @var boolean
+     */
+    public $timestamps = false;
 }
